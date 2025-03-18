0a5c3ca0f83ddf570da36c957c17add0332e08e1
esteb4n14 <83521361+esteb4n14@users.noreply.github.com>
2025-03-18 10:38:20 -0500

    Funcion sorteo agregada

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
 
+//Funcion para agregar amigos
 function agregarAmigo() {
-    let nombreObtenido = document.getElementById("amigo").value;
+    let nombreObtenido = document.getElementById("amigo").value; //value obtiene el valor del input
     nombres.push(nombreObtenido);
     mostrarAmigos();
 } 
 
-console.log(nombres)
     
 
 function mostrarAmigos() {
-    let lista = document.getElementById("listaAmigos");
+    let lista = document.getElementById("listaAmigos"); //no se usa value porque ul solo es un contenedor de sub elementos como li 
     lista.replaceChildren();
 
     for (let nombre of nombres) {
@@ -20,5 +20,13 @@ function mostrarAmigos() {
         lista.appendChild(li);
     }
 
+}
+
+function sortearAmigo() {
+    let lista = document.getElementById("listaAmigos");
+    let amigos = lista.getElementsByTagName("li");
+    let sorteo = Math.floor(Math.random() * amigos.length);
+    let amigoElegido = amigos[sorteo].textContent;
+    document.getElementById("amigoElegido").textContent = `Tu amigo secreto es: ${ amigoElegido }`; 
 }
 
