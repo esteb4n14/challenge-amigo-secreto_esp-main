4450a915696e6b49fe01932fa0b1a9a8b488bfae
esteb4n14 <83521361+esteb4n14@users.noreply.github.com>
2025-03-18 09:27:35 -0500

    función de agregar y listar completada

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+
+function agregarAmigo() {
+    let nombreObtenido = document.getElementById("amigo").value;
+    nombres.push(nombreObtenido);
+    mostrarAmigos();
+} 
+
+console.log(nombres)
+    
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.replaceChildren();
+
+    for (let nombre of nombres) {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    }
+
+}
+
